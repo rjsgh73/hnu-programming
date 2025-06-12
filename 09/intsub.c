aba375edc4e16117d07b542809b237cec394ba8f
rjsgh <rjsgh890@naver.com>
2025-06-12 11:08:41 +0900

    9번 폴더 업데이트

---
+++ 09/intsub.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int get_int();
+
+int get_int(void){
+    int res;
+    printf("1 >>");
+    printf("2 >>");
+    scanf("%d%d",);
+    return res;
+}
+
+int main(void){
+    int n;
+    n = get_int();
+    printf("%d\n",n);
+}
---
+++ 09/intsum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int sum(int, int);
+    int sum(int a, int b)
+    {
+        int res = a + b;
+        return res;
+    }
+    int main(void){
+        int result = sum(10,20);
+        printf("%d\n", result);
+        return 0;
+    }
